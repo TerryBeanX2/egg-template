35d8653c3fa2269041049ea3db0a7e9fc96d3482
袁阳 <yuanyang01@daojia-inc.com>
2019-03-27 19:14:48 +0800

    准备开工

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,6 +5,14 @@ class HomeController extends Controller {
         const { ctx } = this
         await ctx.render('index.html')
     }
+    async about_us () {
+        const { ctx } = this
+        await ctx.render('about-us.html')
+    }
+    async news_list () {
+        const { ctx } = this
+        await ctx.render('news-list.html')
+    }
 }
 
 module.exports = HomeController
--- app/router.js
+++ app/router.js
@@ -4,7 +4,8 @@
 module.exports = app => {
     const { router, controller } = app
     router.get('/', controller.home.index)
-    router.get('/news', controller.news.list)
+    router.get('/about-us', controller.home.about_us)
+    router.get('/news-list', controller.home.news_list)
     router.post('/users/add', controller.users.create)
     router.resources('users', '/users', controller.users)
 }
